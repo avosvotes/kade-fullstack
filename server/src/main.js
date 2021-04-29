83264e6a85e45d0f280e4989cb6912e439b8b84d
arthur <arthur.escriou@gmail.com>
2021-04-29 20:24:17 +0200

    some customization

--- server/src/main.js
+++ server/src/main.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const os = require('os')
 const port = process.env | 4500
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Server running on ${os.hostname()} on port ${port}`)
 })
 
 app.listen(port, () => {
